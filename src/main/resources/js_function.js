0e87586912b1eb88ef25aa1a7a741c06d63aac06
aliakseidudko <aliaksei.dudko@tealium.com>
2023-02-06 16:32:25 +0100

    // add comment

--- src/main/resources/js_function.js
+++ src/main/resources/js_function.js
@@ -1,8 +1,8 @@
 let vehicle_details = hostObject.getVehicleData();
 let vehicle_color = hostObject.getColorAsString();
+console.log("vehicle details: " + JSON.stringify(vehicle_details) + "\n")
 
 try {
-    console.log("vehicle details: " + JSON.stringify(vehicle_details) + "\n")
 
     // assign color using spread syntax
     let vehicle_details_one = {...vehicle_details, ...vehicle_color};
@@ -18,8 +18,10 @@ try {
     // assign to new field
     vehicle_details.new_field = vehicle_color;
     console.log("vehicle details with color using new field: " + JSON.stringify(vehicle_details) + "\n")
+
+    // !!!!ERROR
     // Object.assign
-    Object.assign(vehicle_details, vehicle_color); // com.oracle.truffle.api.dsl.UnsupportedSpecializationException, java string was not convert to TruffleString
+    Object.assign(vehicle_details, vehicle_color); // com.oracle.truffle.api.dsl.UnsupportedSpecializationException, java string was not converted to TruffleString
     console.log("never reach here");
 } catch (ex) {
     console.log(ex);
